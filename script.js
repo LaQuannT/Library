aa95a49b94c4826da6d2bdeccd99210de3d625c8
LaQuannT <117905467+LaQuannT@users.noreply.github.com>
2023-02-07 14:31:10 +0000

    edited js to display form on click and add data to table

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-const addBtn = document.getElementById('add-button');
-const formDiv = document.getElementById('form-input');
-
-// displays form when button is clicked
-addBtn.addEventListener('click', () => {
-  formDiv.style.display = 'block';
-});
-
-// Global var declarations
+// local storage array
 let myLibrary = [];
 
-// defines a object that will stored in array
+// defines a object that will be stored in array
 class Book {
   // the constructor...
   constructor(title, author, pages, read) {
@@ -20,28 +12,44 @@ class Book {
   }
 }
 
-// Takes new object and adds to array
-function addBookToLibrary(book) {
-  // do stuff here
-  myLibrary.push(book);
-}
-
-// Iterate over array on display each index
-function viewBooks() {
-  let list = [];
-  let name;
-  for (let i = 0; i < myLibrary.length; i++) {
-    name = myLibrary[i].title;
-    list.push(name);
-  }
-  document.querySelector('body').append(list);
-}
-
-// Take values from form to add book
-function getBook(nameA, nameB, num, bool) {
+// Takes user input and add book to library
+function addBookToLibrary(nameA, nameB, num, bool) {
   let newBook = new Book(nameA, nameB, num, bool);
-  addBookToLibrary(newBook);
+  myLibrary.push(newBook);
+  bookDisplay();
 }
 
-getBook('The Hobbit', 'J.R.R. Tolkien', 295, 'No');
-viewBooks();
+// Iterate over array on display each book in page table
+function bookDisplay() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    bk.textContent = myLibrary[i].title;
+    writer.textContent = myLibrary[i].author;
+    pgNum.textContent = myLibrary[i].pages;
+    rd.textContent = myLibrary[i].read;
+  }
+}
+
+// selectors for form input values
+const bkName = document.getElementById('book-title').value;
+const bkAuthor = document.getElementById('author').value;
+const bkPages = document.getElementById('pages').value;
+const submit = document.getElementById('submit');
+
+// selectors to add to book table
+const addBtn = document.getElementById('add-btn');
+const form = document.querySelector('form');
+const table = document.querySelector('table');
+let row = table.insertRow(1);
+let bk = row.insertCell(0);
+let writer = row.insertCell(1);
+let pgNum = row.insertCell(2);
+let rd = row.insertCell(3);
+
+// displays form when add book btn is clicked
+addBtn.addEventListener('click', () => {
+  form.style.display = 'flex';
+});
+
+//test  for bug in adding book to table - bug: book just replaces first book
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'no');
+addBookToLibrary('Cause!', 'Jackie & Kevin Freiberg', 195, 'no');
