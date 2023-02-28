9562a7deaf3c0f6f1c0b559e0078654ff8e863f0
LaQuan <117905467+LaQuannT@users.noreply.github.com>
2023-02-28 08:31:13 +0000

    refactored HTML JS & CSS

--- script.js
+++ script.js
@@ -1,106 +1,110 @@
-let myLibrary = [];
+const tbody = document.querySelector('tbody')
+const form = document.querySelector('form')
+const submitBtn = document.querySelector('#btn-submit')
+const addBtn = document.getElementById('add-btn')
+
+let myLibrary = []
 
 // defines a object that will be stored in array
-class Book {
-  // the constructor...
-  constructor(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+// book function on prototype to change read status
+Book.prototype.changeStatus = function () {
+  this.read === 'No' ? (this.read = 'Yes') : (this.read = 'No')
 }
 
 // Takes user input and add book to library
-function addBookToLibrary(nameA, nameB, num, bool) {
-  let newBook = new Book(nameA, nameB, num, bool);
-  myLibrary.push(newBook);
-  bookDisplay(myLibrary);
+function addBookToLibrary(name, writer, num, bool) {
+  let book = new Book(name, writer, num, bool)
+  myLibrary.push(book)
+  displayBooks()
 }
 
-/* Inserts a row and cells to table - iterating over array and displaying each book in page table */
-function bookDisplay(array) {
-  let rNum = array.length;
-  let row = table.insertRow(rNum);
-  let bk = row.insertCell(0);
-  let writer = row.insertCell(1);
-  let pgNum = row.insertCell(2);
-  let rd = row.insertCell(3);
-  let del = row.insertCell(4);
+/* Inserts a row and cells to table - iterating over array and displaying each book on page table */
+function displayBooks() {
+  let row = tbody.insertRow(-1),
+    bk = row.insertCell(0),
+    writer = row.insertCell(1),
+    pgNum = row.insertCell(2),
+    stat = row.insertCell(3),
+    del = row.insertCell(4),
+    update = row.insertCell(5)
 
-  for (let i = 0; i < rNum; i++) {
-    bk.textContent = myLibrary[i].title;
-    writer.textContent = myLibrary[i].author;
-    pgNum.textContent = myLibrary[i].pages;
-    rd.innerHTML = `<button type="button" class="stat-btn">
-        ${myLibrary[i].read}
-      </button>`;
-    del.innerHTML = '<button type="button" class="del-btn">Remove</button>';
+  myLibrary.forEach((book) => {
+    bk.textContent = book.title
+    writer.textContent = book.author
+    pgNum.textContent = book.pages
+    del.innerHTML = '<button type="button" class="del-btn">Remove</button>'
+    update.innerHTML = '<button type="button" class="stat-btn">Update</button>'
+
+    book.read === 'Yes'
+      ? (stat.textContent = 'Read')
+      : (stat.textContent = 'Not Read')
+  })
+}
+
+// displays form when add book btn is clicked
+addBtn.addEventListener('click', () => (form.style.display = 'flex'))
+
+// Event listener taking user input and adding it to table
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+  let bkName = document.querySelector('#book-title').value,
+    bkAuthor = document.querySelector('#author').value,
+    bkPages = document.querySelector('#pages').value,
+    bkStatus = document.querySelector('input[name="status"]:checked').value
+
+  addBookToLibrary(bkName, bkAuthor, bkPages, bkStatus)
+
+  // Clears input fields
+  form.reset()
+
+  // Hides form
+  form.style.display = 'none'
+})
+
+// Listens for del btn for removal of tr
+tbody.addEventListener('click', (event) => {
+  let buffer = event.target.closest('tr'),
+    nameArg = buffer.firstChild.innerHTML
+  // if target clicked has class 'del-btn' remove table row
+  if (event.target.classList.contains('del-btn')) {
+    myLibrary.splice(bookNum(nameArg), 1)
+    buffer.remove()
   }
-}
+  // if target is stat-btn updates read status
+  else if (event.target.classList.contains('stat-btn')) {
+    changeRead(bookNum(nameArg))
+  }
+})
 
 // gets the index of the book
-function bookNum(libraryArray, arg) {
-  if (libraryArray.length <= 0) {
-    return 0;
+function bookNum(arg) {
+  if (myLibrary.length <= 0) {
+    return 0
   }
-  for (i of libraryArray) {
+  for (i of myLibrary) {
     if (i.title === arg) {
-      return libraryArray.indexOf(i);
+      return myLibrary.indexOf(i)
     }
   }
 }
 
 // updates the read status of book
 function changeRead(i) {
-  if (myLibrary[i].read === 'No') {
-    myLibrary[i].read = 'Yes';
-  } else if (myLibrary[i].read === 'Yes') {
-    myLibrary[i].read = 'No';
-  }
+  myLibrary[i].changeStatus()
 }
 
-// DOM element selection
-const btnSubmit = document.querySelector('#btn-submit');
-const addBtn = document.getElementById('add-btn');
-const form = document.querySelector('form');
-const table = document.querySelector('table');
-
-// displays form when add book btn is clicked
-addBtn.addEventListener('click', () => {
-  form.style.display = 'flex';
-});
-
-// Event listener taking user input and adding it to table
-btnSubmit.addEventListener('click', (event) => {
-  event.preventDefault();
-  let bkName = document.querySelector('#book-title').value;
-  let bkAuthor = document.querySelector('#author').value;
-  let bkPages = document.querySelector('#pages').value;
-  let bkStatus = document.querySelector('input[name="status"]:checked').value;
-  addBookToLibrary(bkName, bkAuthor, bkPages, bkStatus);
-
-  // Clears input fields
-  form.reset();
-
-  // Hides form
-  form.style.display = 'none';
-});
-
-// Listens for del btn for removal of tr
-table.addEventListener('click', (event) => {
-  // if target clicked has class 'del-btn' remove table row
-  if (event.target.classList.contains('del-btn')) {
-    event.target.closest('tr').remove();
-  } else if (event.target.classList.contains('stat-btn')) {
-    let buffer = event.target.closest('tr');
-    let nameArg = buffer.firstChild.innerHTML;
-    changeRead(bookNum(myLibrary, nameArg));
-  }
-});
-
 //used to test for bugs
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'No');
-addBookToLibrary('Cause!', 'Jackie & Kevin Freiberg', 195, 'No');
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'No')
+addBookToLibrary('Cause!', 'Jackie & Kevin Freiberg', 195, 'Yes')
 
-/* has a few bugs that need fixing*/
+/* TODO -
+  status of read needs to change on page when clicked (already changes in array)
+  add counter functions amount of books, books read, and books to read
+*/
